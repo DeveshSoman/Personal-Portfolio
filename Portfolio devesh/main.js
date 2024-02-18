205e27e0b630afc1a98475aaa9d0662421dafbc7
DeveshSoman <149900619+DeveshSoman@users.noreply.github.com>
2024-02-18 20:19:00 +0530

    Add files via upload

---
+++ Portfolio devesh/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Mean/Mern Developer", "Full Stack Developer", "MongoDB Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
